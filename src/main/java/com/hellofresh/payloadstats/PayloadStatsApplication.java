bc75640017cff1ab9af76ba13a018ce0a0e94dda
vikas <vikasbanavara@gmail.com>
2021-08-03 11:33:46 +0530

    First commit

---
+++ src/main/java/com/hellofresh/payloadstats/PayloadStatsApplication.java
@@ -0,0 +1,13 @@
+package com.hellofresh.payloadstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayloadStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayloadStatsApplication.class, args);
+	}
+
+}
